32d81ad24b32510dbdafe98bea04faa70a7054db
Arielle Ramirez <ariellenramirez@gmail.com>
2019-02-04 18:58:33 -0500

    allowed movie to toggle

--- src/components/Watch.js
+++ src/components/Watch.js
@@ -1,39 +1,68 @@
 import React, { Component } from "react";
 import NavBar from "../components/NavBar";
 import playButton from "../images/play.png";
+import test from "../videos/test.mov";
 
 class Watch extends Component {
   state = {
-    isClicked: false
+    isClicked: false,
+    movie: true
   };
   handleMovie = event => {
+    if (this.state.movie === false) {
+      this.setState({
+        movie: true
+      });
+    }
     this.setState({
       isClicked: true
     });
   };
+
+  exitMovie = event => {
+    console.log(event.target);
+    this.setState({
+      movie: false
+    });
+  };
   render() {
+    console.log(this.state.movie);
+    console.log(this.state.isClicked);
+
     return (
       <div className="imgbox">
-        <NavBar />
-        <button
-          style={{
-            background: "none",
-            border: "none",
-            width: 10,
-            height: 10,
-            outline: "none",
-            cursor: "pointer",
-            marginLeft: "47%"
-          }}
-          onClick={this.handleMovie}
-          data-video-url="http://www.w3schools.com/html/mov_bbb.mp4"
-        >
-          <img src={playButton} />
-        </button>
-        {this.state.isClicked ? (
-          <video style={{ width: "100%" }} id="player" controls />
-        ) : null}
-        <p className="paragraph">A film created by Erin Ramirez</p>
+        {this.state.isClicked && this.state.movie ? (
+          <video
+            style={{ width: "100%", height: "100vh" }}
+            id="player"
+            onClick={this.exitMovie}
+            autoPlay
+          >
+            <source src={test} type="video/ogg" />
+          </video>
+        ) : (
+          <div>
+            <NavBar />
+            <button
+              style={{
+                background: "none",
+                border: "none",
+                outline: "none",
+                cursor: "pointer",
+                marginTop: "10%",
+                textAlign: "center"
+              }}
+              onClick={this.handleMovie}
+            >
+              <img
+                style={{ width: "50%", height: "50%" }}
+                alt="play"
+                src={playButton}
+              />
+            </button>
+            <p className="paragraph">A film created by Erin Ramirez</p>
+          </div>
+        )}
       </div>
     );
   }
